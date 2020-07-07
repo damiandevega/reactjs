a0b94edfa89f360c78ca84c4873f4fb6edfe9afe
Damian de Vega <damiandevega@gmail.com>
2020-07-06 21:30:45 -0400

    adding styled-components package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -101,4 +100,4 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
