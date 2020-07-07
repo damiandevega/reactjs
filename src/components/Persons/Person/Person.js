ea0468bae01517e6d4ef2b09cccfac09e4e3f8e9
Damian de Vega <damiandevega@gmail.com>
2020-07-07 17:04:15 -0400

    adding aux hoc file

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
 
+import Aux from '../../../hoc/Aux'
+
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering');
     return (
+      <Aux>
       <div className={classes.Person}>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
@@ -12,6 +15,8 @@ class Person extends Component {
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
       </div>
+      <div>hi</div>
+      </Aux>
     );
   }
 };
