d1cf2587010b81a9067a64b1a4b3aa2a2f094af3
Damian de Vega <damiandevega@gmail.com>
2020-07-07 20:28:00 -0400

    adding refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types';
 import classes from './Person.css';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering');
     return (
@@ -14,6 +24,7 @@ class Person extends Component {
           <p>{this.props.children}</p>
           <input
             type="text"
+            ref={this.inputElementRef}
             onChange={this.props.changed}
             value={this.props.name}
           />
