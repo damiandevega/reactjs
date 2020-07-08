e906e0ce312c2b077d85c4408eab635a9efe27f9
Damian de Vega <damiandevega@gmail.com>
2020-07-07 20:02:50 -0400

    adding prop types

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.css';
-
-import Aux from '../../../hoc/Aux'
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering');
     return (
-      <Aux>
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
-          I'm {this.props.name} and I am {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
-      <div>hi</div>
-      </Aux>
+      <Fragment>
+        <div className={classes.Person}>
+          <p onClick={this.props.click}>
+            I'm {this.props.name} and I am {this.props.age} years old!
+          </p>
+          <p>{this.props.children}</p>
+          <input
+            type="text"
+            onChange={this.props.changed}
+            value={this.props.name}
+          />
+        </div>
+      </Fragment>
     );
   }
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func,
 };
 
 export default Person;
